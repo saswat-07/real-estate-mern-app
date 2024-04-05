abfb5a45cfb6a2a4263e3f634cf4d809e8dbad7d
saswat-07 <saswatupadhyayas@gmail.com>
2024-04-05 20:35:12 +0530

    updated firebase configs

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-e482a.firebaseapp.com",
-  projectId: "mern-estate-e482a",
-  storageBucket: "mern-estate-e482a.appspot.com",
-  messagingSenderId: "922093963638",
-  appId: "1:922093963638:web:3abc26298e0ee1dc361257"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
